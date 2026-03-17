463385eadd65d33ac24268f898be7a4bf24a58a4
Vicky-Sharma11 <vickysharma95825@gmail.com>
2026-03-17 18:16:40 +0530

    added missed comments

--- app.sql
+++ app.sql
@@ -7,9 +7,13 @@ FROM salesorder
 GROUP BY customerId
 ;
 
+-- fetching every information from customer table
+
 SELECT
 *
 FROM customer;
+
+-- fetching productId and categoryId from prdouct table
 
 SELECT
 productId,
